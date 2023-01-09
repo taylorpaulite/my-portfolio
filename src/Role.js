6820f146a805f9a293f01360846cd6f28377c945
taylorpaulite <taylor.paulite@gmail.com>
2023-01-09 17:17:48 -0500

    Version 2 changes

---
+++ src/Role.js
@@ -0,0 +1,18 @@
+import React, { Children } from 'react';
+
+//can consider making a skill component
+
+export default function Role({group, children}) {
+    return (
+        <article className="pb-8">
+            <header className="">
+                <h3 className="text-base text-left font-medium text-neutral-800 dark:text-neutral-50">{group}</h3>
+            </header>
+            <div className="">
+                <ul className="font-base text-base text-neutral-600 dark:text-neutral-200 list-disc">
+                    {children}
+                </ul>
+            </div>
+        </article>
+    );
+}
