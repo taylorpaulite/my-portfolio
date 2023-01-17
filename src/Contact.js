455ae0fd836712873b9b75baea3d0cadb7ad6b8c
taylorpaulite <taylor.paulite@gmail.com>
2023-01-16 23:01:11 -0500

    Update to Contact page and Navigation

--- src/Contact.js
+++ src/Contact.js
@@ -2,12 +2,16 @@ import React from 'react'
 
 export default function Contact() {
   return (
-    <div className="block lg:flex lg:flex-row px-12 py-24">
-        <div className="lg:basis-1/2">
-            <h1 className="text-5xl font-medium text-neutral-800 dark:text-neutral-50 text-left pb-4">Thanks for taking the time to reach out.<span className="text-lime-600"> How can I help you today?</span></h1>
-            <p className="text-left text-medium pb-8 text-neutral-600 dark:text-neutral-200">Feel free to send me an e-mail via the form or say hello through direct message through the below platforms.</p>
+    <div className="flex px-12 py-48 justify-center">
+        <div className="max-w-lg">
+            <h1 className="text-5xl font-medium text-neutral-800 dark:text-neutral-50 text-center pb-4">Thanks for taking the time to reach out. 👋 <br/> <br/> Send me an email at<span className="text-lime-600"> taylor.paulite@gmail.com</span></h1>
+            <p className="text-center text-medium pb-8 text-neutral-600 dark:text-neutral-200">If you prefer direct messaging, feel free to say hello through the platforms below.</p>
         </div>
-        <div className="block lg:basis-1/2 content-center lg:pl-12">
+    </div>
+  )
+}
+
+/* <div className="block lg:basis-1/2 content-center lg:pl-12">
             <form name="contact" method="POST" data-netlify="true">
                 <input 
                     type="hidden" 
@@ -27,7 +31,4 @@ export default function Contact() {
                 <textarea name="message" required className="block border rounded border-neutral-200 p-2 pb-12 mb-8 w-full text-neutral-600"></textarea>
                 <button type="submit" key="contact-btn" className="rounded-full block text-left bg-neutral-600 hover:bg-neutral-500 dark:hover:bg-neutral-500 text-neutral-50 text-base px-4 py-2 justify-start">Submit</button>
             </form>
-        </div>
-    </div>
-  )
-}
+        </div> */
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { Link } from "react-router-dom";
+import { HashLink as Link } from 'react-router-hash-link';
+
 import { Bars3Icon } from '@heroicons/react/20/solid';
 import NavModal from './NavModal.js';
 
@@ -13,10 +14,16 @@ export default function Navbar() {
             </Link>
             <div className="md:block md:justify-right">
                 <ul className="hidden list-none md:flex justify-center">
-                    <li key="about-btn" className="text-left text-base font-regular text-neutral-800 hover:text-neutral-600 dark:hover:text-neutral-400 dark:text-neutral-200 px-2 pt-2"><a href="#about">About</a></li>
-                    <li key="portfolio-btn" className="text-left text-base font-regular text-neutral-800 hover:text-neutral-600 dark:hover:text-neutral-400 dark:text-neutral-200 px-2 pt-2"><a href="#portfolio">Portfolio</a></li>
-                    <li key="blog-btn" className="text-left text-base font-regular text-neutral-800 hover:text-neutral-600 dark:hover:text-neutral-400 dark:text-neutral-200 px-2 pt-2"><a href="#blog">Blog</a></li>
-                    <li key="resume-btn" className="text-left text-base font-regular text-neutral-800 hover:text-neutral-600 dark:hover:text-neutral-400 dark:text-neutral-200 pl-2 pr-4 pt-2"><a target="_blank" rel="noreferrer" href="https://acrobat.adobe.com/link/home/">Resume</a></li>
+                    <Link to={"/#about"}>
+                        <li key="about-btn" className="text-left text-base font-regular text-neutral-800 hover:text-neutral-600 dark:hover:text-neutral-400 dark:text-neutral-200 px-2 pt-2">About</li>
+                    </Link>
+                    <Link to={"/#portfolio"}>
+                    <li key="portfolio-btn" className="text-left text-base font-regular text-neutral-800 hover:text-neutral-600 dark:hover:text-neutral-400 dark:text-neutral-200 px-2 pt-2">Portfolio</li>
+                    </Link>
+                    <Link to={"/#blog"}>
+                    <li key="blog-btn" className="text-left text-base font-regular text-neutral-800 hover:text-neutral-600 dark:hover:text-neutral-400 dark:text-neutral-200 px-2 pt-2">Blog</li>
+                    </Link>
+                    <li key="resume-btn" className="text-left text-base font-regular text-neutral-800 hover:text-neutral-600 dark:hover:text-neutral-400 dark:text-neutral-200 pl-2 pr-4 pt-2"><a target="_blank" rel="noreferrer" href="https://acrobat.adobe.com/id/urn:aaid:sc:US:ed2ef695-c69a-42b2-8f52-c8272e0edc2e">Resume</a></li>
                     <Link to={"/contact"}>
                         <li key="contact-btn" className="rounded-full text-left bg-neutral-600 hover:bg-neutral-500 dark:hover:bg-neutral-500 text-neutral-50 text-base px-4 py-2 justify-start">Say hello 👋</li>
                     </Link>
